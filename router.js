f855b1aaaf024733aa961caca847fbe4b302f536
vladislav99dev <vladislavcho99@abv.bg>
2023-04-05 23:27:10 +0300

    fix

--- router.js
+++ router.js
@@ -1,23 +1,18 @@
-const router = require('express').Router();
-const isAdmin = require('./middlewares/isAdmin');
-const userController = require('./controllers/userController');
-const adminController = require('./controllers/adminController');
-const productsController = require('./controllers/productsController');
+const router = require("express").Router();
+const isAdmin = require("./middlewares/isAdmin");
+const userController = require("./controllers/userController");
+const adminController = require("./controllers/adminController");
+const productsController = require("./controllers/productsController");
 const ordersController = require("./controllers/ordersController");
 
+router.use("/users", userController);
+router.use("/admin", isAdmin, adminController);
+router.use("/products", productsController);
+router.use("/orders", ordersController);
 
-router.use('/',(req,res)=> {
-    res.status(200).json({message: "Server hit"})
-})
-// router.use('/users',userController)
-// router.use('/admin',isAdmin,adminController)
-// router.use('/products',productsController)
-// router.use('/orders',ordersController)
-
-router.all('*', (req,res) => {
-    res.status(404).json({error:'This service does not exist!'})
-})
-
+router.all("*", (req, res) => {
+  res.status(404).json({ error: "This service does not exist!" });
+});
 
 module.exports = router;
 
