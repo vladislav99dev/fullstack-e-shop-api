a9eddf0bf903d354a7b6cea475e9288be2d1c23c
vladislav99dev <vladislavcho99@abv.bg>
2023-04-05 21:09:05 +0300

    test

--- api.js
+++ api.js
@@ -1,17 +1,19 @@
 const express = require("express");
-const dotenv = require('dotenv')
-const router = require('./router')
-const cors = require('cors')
+const dotenv = require("dotenv");
+const router = require("./router");
+const cors = require("cors");
 const databaseConnect = require("./config/databaseConnect");
 
 const app = express();
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
-app.use(router);
+router.use("/products", (req, res) => {
+  res.status(200).json({ message: "You hit products controller" });
+});
+// app.use(router);
 dotenv.config();
-
 
 databaseConnect(process.env.DB_CONNECTION_STRING)
   .then((response) => {
