9db457822630107ea63d0ca7bc8ff034cb11c68e
vladislav99dev <vladislavcho99@abv.bg>
2023-04-05 20:46:46 +0300

    change router

--- api.js
+++ api.js
@@ -9,7 +9,10 @@ const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cors());
-app.use(router);
+// app.use(router);
+app.use('/',(req,res) => {
+  return res.status(200).json({message: "Hello from server"})
+})
 dotenv.config();
 
 
